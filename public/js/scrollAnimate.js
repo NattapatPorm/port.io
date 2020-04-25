d5f77bf87e4e616c3201664dd2a21884be710ac7
NattapatPorm <pigporm@gmail.com>
2020-04-25 17:04:28 +0700

    first commit ครั้งแรก

---
+++ public/js/scrollAnimate.js
@@ -0,0 +1,24 @@
+$(document).ready(function (){
+    $("#click-join-my-port").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+});
+
+$(document).ready(function (){
+    $("#click-about").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+});
+
+
+$(document).ready(function (){
+    $("#click-experience").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#experience").offset().top
+        }, 1000);
+    });
+});
